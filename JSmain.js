305f47832621d5fea2ce329f51984ae1ce4c7489
barbarekh <barbarekhalvashi2@gmail.com>
2024-05-20 21:34:16 +0400

    brbr

---
+++ JSmain.js
@@ -0,0 +1,141 @@
+const user = {
+    name: 'Babi',
+    lastName: 'Khalvashi',
+    userName: '456',
+    move: function(){
+        console.log('Barbare is running')
+    }
+}
+console.log(user.name,user.lastName,user.userName)
+user.move()
+
+
+
+
+
+console.log("hello")
+
+// let myName="barbare"
+// myName='brbr'
+
+// const myNumber = 10
+// const mystrNumber = "10"
+// const myBoolean = false
+
+// console.log(myNumber === mystrNumber)
+
+// console.log(typeof myName)
+// console.log(typeof myNumber)
+// console.log(typeof mystrNumber)
+// console.log(typeof myBoolean)
+
+// const c = 9
+// const d = 5
+
+// if(c+d === 8){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+
+// function calculate(num1,num2){
+//     return num1 + num2
+// }
+
+// console.log(calculate(100,130))
+
+
+// function calculate1(num1,num2){
+//     if(typeof num1 === "string"){
+//         return false
+//     }
+//     if(typeof num2 === "string"){
+//         return false
+//     }
+//     return num1 + num2
+// }
+// console.log(calculate (105,90))
+// console.log(calculate1("brbr,100"))
+
+
+
+
+
+// //homework1//
+// function calculate2(a,b){
+//     if( a === b ){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+
+// }
+// console.log(calculate2(10,10))
+
+
+// function calculate3(d,c){
+//     if( d === c ){
+//         return true
+//     }
+//     else {
+//         return false
+//     }
+
+// }
+
+// console.log(calculate3("baabi",9))
+
+// function calculate3(d,c){
+//     if(typeof d === "string" ){
+//         return false
+//     }
+//     if(typeof c === "string"){
+//         return false
+//     }
+//     if(typeof d === "number"){
+//         return true
+//     }
+//     if(typeof c === "number"){
+//         return true
+//     }
+// }
+// console.log(calculate3(789,123))
+
+
+// // homework2
+// function toC(F){
+//     if(typeof F !== "number"){
+//         return false
+//     }
+//     return (F - 32) * 5/9
+// }
+// let F = 100
+// console.log(toC(F)) 
+
+// //homwork3//
+// function calculate(a1,b2){
+//     if(typeof a1 === "string"){
+//         return false
+//     }
+//     if(typeof b2 === "string"){
+//         return false
+//     }
+//     else{
+//         return a1 - b2
+//     }
+// }
+// console.log(calculate(1352,876))
+
+
+// const user = {
+//     name: 'Babi',
+//     lastName: 'Khalvashi',
+//     userName: '456',
+//     move: function(){
+//         console.log('Barbare is running')
+//     }
+// }
+// console.log(user.name,user.lastName,user.userName)
+// user.move()
---
+++ JSmain2.js
@@ -0,0 +1,11 @@
+let arr = [{name: 'Temo', age: 25},
+{name: 'Lasha', age: 21}, {name: 'Ana', age: 28}]
+
+let index = arr[0].age
+
+for(let obj of arr){
+    if(obj.age<index)
+    index = obj.age
+}
+console.log(index)
+
